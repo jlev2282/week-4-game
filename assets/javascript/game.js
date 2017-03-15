f7fbb05e6f7677ec413a377ea5816c4d219de254
John <jlev2282@gmail.com>
2017-03-15 15:21:46 -0400

    added the scorebox logic and initial game winlose logic

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,8 +1,9 @@
 $(document).ready(function() {
-
+//Define the variables for the minerals//
 var oreArray = ['#golore','#silore','#titore','#diaore'];
 var scoreArray = [];
 
+// state function to start game and set values for ores and goal number//
 $('#start').on('click', function(){
 	for (var i=0; i<4; i++) {
 		var oreval = Math.floor(Math.random() * 11) +1;
@@ -12,10 +13,19 @@ $('#start').on('click', function(){
 	var rannum = Math.floor(Math.random() * 120) +1;
 	console.log(rannum);
 	$('#goalnum').html(rannum);
+	$('#goalnum').attr('value',rannum);
+	});
+//state function that logics scoreboard
+$('.ore').on('click', function() {
+	var score = (this.value);
+	//$('scoreArray').push(score);
+	$('#scorebox').html(scoreArray);
 	});
 
-$('.ore').on('click', function() {
-	$(this.value).html('#scorebox');
-	});
+//state the logic for winning and losing
+if ('#scorebox.value'='#goalnum.value') {
+	alert(You win!)
+	 };
+}
 
 });
