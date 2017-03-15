867f4b3d7ab7df45acb9906f4c1cb5e260e2c06e
John <jlev2282@gmail.com>
2017-03-15 18:22:37 -0400

    updated logic to reset game and alert wins and losses

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -2,16 +2,19 @@ $(document).ready(function() {
 //Define the variables for the minerals//
 var oreArray = ['#golore','#silore','#titore','#diaore'];
 var scoreArray = [];
-var score = '';
+var score = 0;
+var wins = 0;
+var losses = 0;
 
 // state function to start game and set values for ores and goal number//
 $('#start').on('click', function(){
+	$('#scorebox').html("0");
 	for (var i=0; i<4; i++) {
 		var oreval = Math.floor(Math.random() * 11) +1;
 		console.log(oreval);
 		$(oreArray[i]).attr('value', oreval);
 		}
-	var rannum = Math.floor(Math.random() * 120) +1;
+	rannum = Math.floor(Math.random() * 120) +1;
 	console.log(rannum);
 	$('#goalnum').html(rannum);
 	$('#goalnum').attr('value',rannum);
@@ -21,13 +24,23 @@ $('#start').on('click', function(){
 $('.ore').on('click', function() {
 	var total = (this.value);
 	scoreArray.push(parseInt(total));
-
-	for ( var i = 0; i < scoreArray.length; i++ ){
-    	score += scoreArray[i]
-	};
+	score += scoreArray[scoreArray.length - 1];
+	// for ( var i = 0; i < scoreArray.length; i++ ){
+ //    	score += scoreArray[i]
+	// };
     
 	$('#scorebox').html(score);
 
+	if (rannum===score){
+		alert("You Win!");
+		wins++;
+		$('#wins').html(wins);
+		}
+	if (score>rannum){
+		alert("You Lose!");
+		losses++;
+		$('#losses').html(losses);
+	}
 	});
 //state the logic for winning and losing
 /*if ('#scorebox.value'='#goalnum.value') {
