59ff33242734a07e9ce80f0265a7443e16ef410b
John <jlev2282@gmail.com>
2017-03-15 11:53:48 -0400

    added logic for random goal number

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,11 +1,21 @@
+$(document).ready(function() {
 
 var oreArray = ['#golore','#silore','#titore','#diaore'];
+var scoreArray = [];
 
 $('#start').on('click', function(){
-	for (var i=0; i<3; i++) {
-		var oreval = Math.floor(Math.random() * 11) =1;
+	for (var i=0; i<4; i++) {
+		var oreval = Math.floor(Math.random() * 11) +1;
 		console.log(oreval);
 		$(oreArray[i]).attr('value', oreval);
 		}
-
+	var rannum = Math.floor(Math.random() * 120) +1;
+	console.log(rannum);
+	$('#goalnum').html(rannum);
 	});
+
+$('.ore').on('click', function() {
+	$(this.value).html('#scorebox');
+	});
+
+});
